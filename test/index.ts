d2bba40798c8ded1facf7497a947ef2a3af9206f
xjxujing <xujing_xj@foxmail.com>
2021-05-09 15:04:51 +0800

    使用 sinon 测试函数

--- test/index.ts
+++ test/index.ts
@@ -1,4 +1,7 @@
 import * as chai from 'chai'
+import * as sinon from 'sinon'
+import * as sinonChai from 'sinon-chai'
+chai.use(sinonChai)
 
 const assert = chai.assert
 import Promise from '../src/Promise'
@@ -31,37 +34,41 @@ describe('Promise', () => {
 
 
   it('new Promise(fn) 中的 fn 立即执行', () => {
-    let called = false
-    new Promise(() => {
-      called = true
-    })
-    // 如果执行了 called 就不再是 false 了
-    assert.isNotFalse(called)
+    // 老方法
+    // let called = false
+    // new Promise(() => {
+    //   called = true
+    // })
+    // // 如果执行了 called 就不再是 false 了
+    // assert.isNotFalse(called)
+
+    // 使用 sinon, 更语义化
+    const fn = sinon.fake() // 生成一个假的函数
+    new Promise(fn)
+    assert(fn.called) // 判断函数有没有执行
   })
 
-  it('new Promise(fn) 中的 fn, 执行的时候接受 resolve 和 reject 两个函数', () => {
+  it('new Promise(fn) 中的 fn, 执行的时候接受 resolve 和 reject 两个函数', done => {
     new Promise((resolve, reject) => {
       assert.isFunction(resolve)
       assert.isFunction(reject)
+      done() // 确保 assert 运行了，再结束
     })
   })
 
 
-  it('promise.then(sucess) 里面的 sucess' + 
-  '会在 resolve 执行的时候执行', done => {
-    let called = false
+  it('promise.then(sucess) 里面的 sucess' +
+    '会在 resolve 执行的时候执行', done => {
 
-    const promise = new Promise((resolve, reject) => {
-      assert(called === false)
-      resolve()
-      setTimeout(() => {
-        assert(called === true)
-        done()
+      const success = sinon.fake()
+      const promise = new Promise((resolve, reject) => {
+        assert.isFalse(success.called) // sucess 还没有调用
+        resolve()
+        setTimeout(() => {
+          assert.isTrue(success.called) // sucess 调用过了
+          done()
+        })
       })
+      promise.then(success)
     })
-
-    promise.then(() => {
-      called = true
-    })
-  })
 })
